1630405ccebef71d67836ca27470aa1503a93576
Serhii Kuzmychov <ku3mich@gmail.com>
2021-05-24 12:16:32 +0300

    added Empty

--- Enumerable.Items/EnumerableExtensions.cs
+++ Enumerable.Items/EnumerableExtensions.cs
@@ -3,12 +3,15 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace System.Linq
 {
-    public static class ItemsExtensions
+    public static class EnumerableExtensions
     {
         public static void ForEach<T>([NotNull] this IEnumerable<T> items, Action<T> action)
         {
             foreach (var e in items)
                 action(e);
         }
+
+        public static bool Empty<T>([NotNull] this IEnumerable<T> items)
+            => !items.Any();
     }
 }
